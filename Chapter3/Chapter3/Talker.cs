f823fa723520fe187c14c430633b02ece0907998
Rommel Rico <rommeltj@gmail.com>
2013-08-12 22:38:28 -0700

    Chapter3

---
+++ Chapter3/Chapter3/Talker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter3
+{
+    class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int numberOfTimes)
+        {
+            string finalString = "";
+            for (int i = 1; i <= numberOfTimes; i++)
+            {
+                finalString += thingToSay + "\n";
+            }
+            MessageBox.Show(finalString);
+            return finalString.Length;
+        }
+    }
+}
---
+++ FunWithJoeAndBob/FunWithJoeAndBob/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithJoeAndBob
+{
+    public partial class Form1 : Form
+    {
+
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            joe = new Guy();
+            bob = new Guy();
+            joe.Name = "Joe";
+            joe.Cash = 50;
+            bob.Name = "Bob";
+            bob.Cash = 100;
+            updateForm();
+        }
+
+        public void updateForm()
+        {
+            joesCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+            joeGivesToBob.Text = "Joe gives $10 to Bob.";
+            bobGivesToJoe.Text = "Bob gives $5 to Joe.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.receiveCash(10);
+                updateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.giveCash(5);
+            updateForm();
+        }
+
+        private void joeGivesToBob_Click(object sender, EventArgs e)
+        {
+            bob.receiveCash(joe.giveCash(10));
+            updateForm();
+        }
+
+        private void bobGivesToJoe_Click(object sender, EventArgs e)
+        {
+            joe.receiveCash(bob.giveCash(5));
+            updateForm();
+        }
+
+
+    }
+}
